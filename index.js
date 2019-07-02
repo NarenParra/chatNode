438b17d2cfa0bca7eaa5275d2b21c948f590adef
NarenParra <naren21p@gmail.com>
2019-07-01 22:58:13 -0500

    version 0.0.1

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+	io = require("socket.io").listen(server);
+	nicknames={};
+
+server.listen(8000, () => console.log('server on port 8000'));
+//server.listen(process.env.PORT, process.env.IP);
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + '/index.html');
+});
+
+//Sokets
+io.sockets.on('connection', function(socket) {
+	socket.on('send message', function(data) {
+		io.sockets.emit('new message', {msg: data});
+	});
+
+    socket.on('new user', function(data, callback) {
+        if (data in nicknames) {
+            callback(false);
+        } else {
+            callback(true);
+            socket.nickname = data;
+            nicknames[socket.nickname] = 1;
+            updateNickNames();
+        }
+    });
+	function updateNickNames(){
+		io.sockets.emit('usernames', nicknames);
+	}
+});
+
